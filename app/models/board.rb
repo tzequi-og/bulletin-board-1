0bbfa16995051a4963b2afc33e48e76c11773c76
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-11-15 02:59:24 +0000

    boards_posting

---
+++ app/models/board.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+  
+  has_many(:posts, class_name: "Post", foreign_key: "board_id")
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  expires_on :date
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :integer
+#
+class Post < ApplicationRecord
+
+  belongs_to(:board, class_name: "Board", foreign_key: "board_id")
+
+end
